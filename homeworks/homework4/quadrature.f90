42cfd545b488ce3ac005daa5a1e0769c17b0754a
weiweiutd <weiweiamazon@gmail.com>
2015-07-06 15:56:20 -0600

    just finish writing codes

---
+++ homeworks/homework4/quadrature.f90
@@ -0,0 +1,53 @@
+! this module contains a function trapzoid and a subroutin error_table
+module quadrature
+
+contains
+
+real(kind=8) function trapezoid(f,a,b,n)
+    ! This funciton returns the estimate of the integral
+
+    implicit none
+    real(kind=8), intent(in)::a,b
+    real(kind=8),external::f
+    integer, intent(in)::n
+
+    integer :: j
+    real(kind=8):: h,sumf,xj
+
+    h = (b - a)/(n - 1)
+    sumf =  -0.5d0*(f(a)+f(b))
+
+    for j = 2,n-1
+        xj = a + (j-1)*h
+        sumf = sumf + f(xj)
+        enddo
+
+    trapezoid = h*sumf
+
+end function trapezoid
+
+subroutine error_table(f,a,b,nvals,int_true)
+    implicit none   
+    integer,dimension(:),intent(in) :: nvals
+    real(kind=8),external::f
+    real(kind=8),intent(in)::a,b,int_true
+    real(kind=8)::sum,,int_trap,ratio, error, last_error
+    
+    integer::j,n
+    
+    print *, "    n       trapezoid      error        ratio"
+    
+    last_error = 0.d0
+    for j = 1,size(nvals)
+        int_trap = trapezoid(f,a,b,n)
+        error = abs(int_trap - int_true)
+        ratio = last_error/error
+        last_error = error
+        print 11, n, int_trap, error, ratio
+11      format(i8, es22.14,es13.3,es13.3)
+        enddo
+
+end subroutine error_table
+
+end module quadrature
+
